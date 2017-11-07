91c8b15a3b2edb00d091013ee927f932c4c97fdb
Nicholas Yue <yue.nicholas@gmail.com>
2017-11-06 20:36:17 -0800

    Initial commit with CMake configuration

---
+++ src/conrec.h
@@ -0,0 +1,9 @@
+int conrec(double **d,
+           int ilb,
+           int iub,
+           int jlb,
+           int jub,
+           double *x,
+           double *y,
+           int nc,
+           double *z);
